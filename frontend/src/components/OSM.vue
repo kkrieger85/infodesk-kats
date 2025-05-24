a2c47a5e3e7fe4ed72663ce4fae221f23ba411ce
Kevin Krieger <kkrieger85@gmail.com>
2025-05-24 14:52:11 +0200

    add iconpicker

--- frontend/src/components/OSM.vue
+++ frontend/src/components/OSM.vue
@@ -1,5 +1,5 @@
 <template>
-    <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
+    <l-map ref="mapRef" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
         <l-tile-layer url="http://localhost:8080/tile/{z}/{x}/{y}.png" layer-type="base"
             name="Stadia Maps Basemap"></l-tile-layer>
         <l-marker :lat-lng="center">
@@ -8,22 +8,71 @@
         <l-marker :lat-lng="centerSammel">
             <l-icon :icon-url="iconSammelpunktUrl" :icon-size="iconSize" />
         </l-marker>
+        <l-marker
+            v-for="(marker, idx) in customMarkers"
+            :key="idx"
+            :lat-lng="marker.latlng"
+        >
+            <l-icon :icon-url="marker.iconUrl" :icon-size="iconSize" />
+        </l-marker>
+        <l-control position="bottomleft" >
+            <button @click="selectCustomIcon">
+            Add custom marker</button>
+        </l-control>
     </l-map>
+    <MultiselectIconPicker
+  v-if="showIconPicker"
+  @close="showIconPicker = false"
+  @select="addCustomMarker"
+/>
 </template>
 
 
 <script setup>
 import "leaflet/dist/leaflet.css"
-import { LMap, LTileLayer, LMarker, LIcon } from "@vue-leaflet/vue-leaflet"
+import MultiselectIconPicker from "./MultiselectIconPicker.vue"
+import { ref } from "vue"
+import { LMap, LTileLayer, LMarker, LIcon, LControl } from "@vue-leaflet/vue-leaflet"
 
+let showIconPicker = ref(false)
+let selectedIcon = ref(null)
 
-let zoom = 18
-let center = [49.2725, 7.0336]
+let zoom = ref(18)
+let center = ref([49.2725, 7.0336])
 let centerSammel = [49.272865, 7.034289]
 
 let iconExplosionUrl = "/src/assets/taktische_zeichen/Gefahren/Akute_Gefahr_durch_Explosion.svg"
 let iconSammelpunktUrl = "/src/assets/taktische_zeichen/Einrichtungen/Sammelstelle.svg"
+let iconOtherUrl = "/src/assets/taktische_zeichen/Einrichtungen/Andere.svg" // Beispiel für weiteres Icon
+
 let iconSize = [50, 50];;
+
+let customMarkers = ref([])
+let mapRef = ref(null)
+
+
+function selectCustomIcon() {
+    showIconPicker.value = true
+}
+
+function addCustomMarker(icon) {
+    // Einfache Auswahl per prompt (besser: eigenes Modal)
+    showIconPicker.value = false
+    if (!icon) return
+    selectedIcon.value = icon
+
+    // Nutzer klickt auf die Karte, um Position zu wählen
+    const mymap = mapRef.value.leafletObject
+    const onClick = (e) => {
+        customMarkers.value.push({
+            latlng: [e.latlng.lat, e.latlng.lng],
+            iconUrl: icon.url
+        })
+        mymap.off('click', onClick)
+    }
+    mymap.once('click', onClick)
+    //alert("Klicke auf die Karte, um den Marker zu platzieren.")
+}
 
 </script>
 
