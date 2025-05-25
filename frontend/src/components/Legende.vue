01652bc41a6f2028f77e1ea8b087154436cf16d2
Kevin Krieger <kkrieger85@gmail.com>
2025-05-25 11:39:25 +0200

    Refactor Legende component to use uniqueMarkers for rendering and improve marker filtering logic

--- frontend/src/components/Legende.vue
+++ frontend/src/components/Legende.vue
@@ -5,7 +5,7 @@
         </div>
 
         <div class="grid gap-1  grid-cols-3 md:grid-cols-4 lg:grid-cols-5" id="accordion-collapse-body-1">
-            <LegendeItem v-for="(marker, idx) in customMarkers" :key="idx" :imgsrc="marker.iconUrl" :text="marker.name">
+            <LegendeItem v-for="(marker, idx) in uniqueMarkers" :key="idx" :imgsrc="marker.iconUrl" :text="marker.name">
             </LegendeItem>
         </div>
 
@@ -15,6 +15,16 @@
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import LegendeItem from './LegendeItem.vue'
 
-defineProps({ customMarkers: Array })
+import { computed } from 'vue'
+const props = defineProps({ customMarkers: Array })
 
+const uniqueMarkers = computed(() => {
+    const seen = new Set()
+    return (props.customMarkers || []).filter(marker => {
+        const key = marker.iconUrl + '|' + marker.name
+        if (seen.has(key)) return false
+        seen.add(key)
+        return true
+    })
+})
 </script>
